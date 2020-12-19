cfb53c257ec78f94fd6ceb8f819658660abc7862
Christian Johann Bayerle <christian.bayerle@gmail.com>
2020-12-19 21:25:02 +0100

    Artists overview

--- app.py
+++ app.py
@@ -299,20 +299,12 @@ def delete_venue(venue_id):
 
 #  Artists
 #  ----------------------------------------------------------------
+"""Artists overview"""
 @app.route('/artists')
 def artists():
-  # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
-  return render_template('pages/artists.html', artists=data)
+    #Artists overview list, each artist is unique, no grouping as Venues
+    data = Artist.query.order_by('id').all()
+    return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
