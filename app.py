20e4105105eaadcd9de48d7e4bca46e3456035a2
Christian Johann Bayerle <christian.bayerle@gmail.com>
2020-12-21 22:47:16 +0100

    Upcoming and past shows

--- app.py
+++ app.py
@@ -168,32 +168,48 @@ def search_venues():
 def show_venue(venue_id):
     # shows the venue page with the given venue_id
     # HTML needs dict
-    data = Venue.query.filter_by(id=venue_id).all()[0]
+    data = {}
+    venue = Venue.query.filter_by(id=venue_id).one()
 
-    data1={
-        "id": 1,
-        "name": "The Musical Hop",
-        "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-        "address": "1015 Folsom Street",
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "123-123-1234",
-        "website": "https://www.themusicalhop.com",
-        "facebook_link": "https://www.facebook.com/TheMusicalHop",
-        "seeking_talent": True,
-        "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-        "past_shows": [{
-        "artist_id": 4,
-        "artist_name": "Guns N Petals",
-        "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-        "start_time": "2019-05-21T21:30:00.000Z"
-        }],
-        "upcoming_shows": [],
-        "past_shows_count": 1,
-        "upcoming_shows_count": 0,
-    }
+    #Add past and upcoming shows
+    shows = Show.query.filter_by(venue_id=venue_id).all()
+    upcoming_shows = []
+    past_shows = []
+    now = datetime.now()
 
+    for show in shows:
+        temp = {
+            "artist_image_link" : show.artists.image_link,
+            "artist_id" : show.artist_id,
+            "artist_name" : show.artists.name,
+            "start_time" : show.start_time.strftime("%Y-%m-%d %H:%M")
+        }
+        #Check if upcoming or past
+        if show.start_time < now:
+            past_shows.append(temp)
+        else:
+            upcoming_shows.append(temp)
+
+    data={
+        "id": venue.id,
+        "name": venue.name,
+        "genres": venue.genres,
+        "address": venue.address,
+        "city": venue.city,
+        "state": venue.state,
+        "phone": venue.phone,
+        "website": venue.website,
+        "facebook_link": venue.facebook_link,
+        "seeking_talent": venue.seeking_talent,
+        "seeking_description": venue.seeking_description,
+        "image_link": venue.image_link,
+        }
+
+    data["upcoming_shows_count"] = len(upcoming_shows)
+    data["past_shows_count"] = len(past_shows)
+
+    data["upcoming_shows"] = upcoming_shows
+    data["past_shows"] = past_shows
 
     return render_template('pages/show_venue.html', venue=data)
 
@@ -277,37 +293,45 @@ def search_artists():
 def show_artist(artist_id):
     # shows the venue page with the given venue_id
     # HTML needs dict
-    data = Artist.query.filter_by(id=artist_id).all()[0]
+    data = {}
+    artist = Artist.query.filter_by(id=artist_id).one()
 
-    data3={
-        "id": 6,
-        "name": "The Wild Sax Band",
-        "genres": ["Jazz", "Classical"],
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "432-325-5432",
-        "seeking_venue": False,
-        "image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-        "past_shows": [],
-        "upcoming_shows": [{
-          "venue_id": 3,
-          "venue_name": "Park Square Live Music & Coffee",
-          "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-          "start_time": "2035-04-01T20:00:00.000Z"
-        }, {
-          "venue_id": 3,
-          "venue_name": "Park Square Live Music & Coffee",
-          "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-          "start_time": "2035-04-08T20:00:00.000Z"
-        }, {
-          "venue_id": 3,
-          "venue_name": "Park Square Live Music & Coffee",
-          "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-          "start_time": "2035-04-15T20:00:00.000Z"
-        }],
-        "past_shows_count": 0,
-        "upcoming_shows_count": 3,
+    #Add past and upcoming shows
+    shows = Show.query.filter_by(artist_id=artist_id).all()
+    upcoming_shows = []
+    past_shows = []
+    now = datetime.now()
+
+    for show in shows:
+        temp = {
+            "venue_image_link" : show.venues.image_link,
+            "venue_id" : show.venue_id,
+            "venue_name" : show.venues.name,
+            "start_time" : show.start_time.strftime("%Y-%m-%d %H:%M")
+        }
+
+        if show.start_time < now:
+            past_shows.append(temp)
+        else:
+            upcoming_shows.append(temp)
+
+    data={
+        "id": artist.id,
+        "name": artist.name,
+        "genres": artist.genres,
+        "city": artist.city,
+        "state": artist.state,
+        "phone": artist.phone,
+        "seeking_venue": artist.seeking_venue,
+        "seeking_description": artist.seeking_description,
+        "image_link": artist.image_link
     }
+
+    data["upcoming_shows_count"] = len(upcoming_shows)
+    data["past_shows_count"] = len(past_shows)
+
+    data["upcoming_shows"] = upcoming_shows
+    data["past_shows"] = past_shows
 
     return render_template('pages/show_artist.html', artist=data)
 
@@ -423,8 +447,6 @@ def shows():
             "venue_id" : show.venue_id,
             "venue_name" : show.venues.name
         })
-
-
 
 
     datas=[{
